9d01032ed0bc53fada918fac818b826d895e505a
Vaibhav Bhasin <75664061+Vaibhav-Bhasin@users.noreply.github.com>
2021-11-01 14:39:50 +0530

    Update JAVASCRIPT.js

--- JAVASCRIPT.js
+++ JAVASCRIPT.js
@@ -30,13 +30,9 @@ foo4 = [5, 'five', '5']
 
 //alert(1==1);
 /*
-
 alert(1 != "1");
 alert(1 !== "1");
 // ===  also check the data type along with the value
-
-
-
 // Mathematical op
 var a = 2;
 alert(a);
@@ -47,7 +43,6 @@ a--;
 alert(a); 
 */
 /*
-
 // if else elif
 if (2==3)
 {
@@ -60,24 +55,19 @@ alert("okay");
 else{
 alert("Its FAlse");
 }
-
 // loops //
-
 for( var i = 0; i <= foo4.length; i++)
 {
 alert(i);
-
 }
 */
 /*
-
 // functions //
 alert(4);
 confirm("Okay?");
 prompt("What is this?");
 var a = Date();
 alert(a);
-
 */
 // setTimeout(Date(),5000);
 var b = parseInt("123");
@@ -128,6 +118,44 @@ function myFunction()
 alert("You just clickedthe webpage");
 }
 
-#window.addEnentListner = ("click", myFunction());
-
+//window.addEventListner = ("click", myFunction());
+//DOM(Document Object Module)
 // button that changes to dark theme and buton that 
+function change_theme()
+{
+comment("Do you want to change to dark theme");
+}
+//window.addEventListner = ("click", change_theme());
+function light()
+{
+var x = document.getElementsByTagName("h1")[0].innerHTML;
+console.log(x);
+var y = document.getElementById("footer").innerHTML;
+console.log(y);
+var w = document.getElementsByClassName("intro")[0].innerHTML;
+console.log(w);
+document.getElementsByClassName("intro")[0].style.color="red";
+document.getElementsByClassName("intro")[0].style.backgroundColor="black";
+}
+function dark (){
+document.body.style.backgroundColor="black";
+document.body.style.color="white";
+var newH = document.createElement("h");
+
+var myText = document.createTextNode("This is Gothem");
+var d = document.getElementById("myDiv");
+newH.appendChild(myText);
+d.appendChild(newH);
+}
+function light (){
+document.body.style.backgroundColor="white";
+document.body.style.color="black";
+
+var s = document.getElementsByTagName("a")[0]
+s.setAttribute("href","www.facebook.com");
+var m = s.getAttribute("href");
+console.log(m);
+s.innerHTML = m;
+}
+
+// add in
